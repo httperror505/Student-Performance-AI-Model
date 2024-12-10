99c675d7656962fbd8d388c8bfa522891e502004
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2024-12-10 11:44:19 +0800

    Update index.py

--- index.py
+++ index.py
@@ -52,4 +52,4 @@ def upload_plot():
     return jsonify({'message': 'File uploaded successfully', 'file_path': file_path}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
